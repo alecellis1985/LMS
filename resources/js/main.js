e767fe1fd8038fb3dc876c73dde76754c2392c0a
MSI\Alexander <alecellis1985@gmail.com>
2017-02-27 11:31:20 -0300

    index updated nice

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,21 @@ var mainctrl = (function () {
   var ctrlFns = {
 
   };
+  init();
 
+  function init() {
+
+    var itemsToFlip = ["#card"];
+    var totalItems = 12;
+    while (totalItems--) {
+      itemsToFlip.push(totalItems);
+    }
+
+    $(itemsToFlip.join(",#card")).flip({
+      axis: 'y',
+      trigger: 'hover'
+    });
+  }
   return ctrlFns;
 
 })();
